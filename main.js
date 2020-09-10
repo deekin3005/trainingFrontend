a26bf878c7c81355779a6a27d4d3490286e54702
deekin3005 <deekin3005@gmail.com>
2020-09-10 17:40:48 +0400

    1

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const bodyBcg = document.querySelector('body');
 const bgButton = document.querySelector('.bg-button');
+const rgbNumbers = document.querySelector('.rgb-numbers');
 
 bgButton.addEventListener('click',bgColor);
 
 function bgColor (){
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let blue = Math.floor(Math.random()*255)
+
     bodyBcg.style.backgroundColor =
-    'rgb('+ Math.floor(Math.random()*255) + ',' +
-    Math.floor(Math.random()*255) + ',' +
-    Math.floor(Math.random()*255) +')';
+    'rgb('+red + ',' + green + ',' + blue +')';
+
+    rgbNumbers.innerHTML =
+    red + ', ' + green + ', ' + blue;
 }
