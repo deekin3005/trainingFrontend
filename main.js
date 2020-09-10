43cde963979430bae6ba0820f5bac25c356f30b2
deekin3005 <deekin3005@gmail.com>
2020-09-10 18:59:39 +0400

    1

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-const bodyBcg = document.querySelector('body');
-const bgButton = document.querySelector('.bg-button');
+// RGB block work
+
+const rgbBlock = document.querySelector('.rgb-block');
+const rgbButton = document.querySelector('.rgb-button');
 const rgbNumbers = document.querySelector('.rgb-numbers');
 
-bgButton.addEventListener('click',bgColor);
+rgbButton.addEventListener('click', rgbColorChange);
 
-function bgColor (){
+function rgbColorChange(){
     let red = Math.floor(Math.random()*255)
     let green = Math.floor(Math.random()*255)
     let blue = Math.floor(Math.random()*255)
 
-    bodyBcg.style.backgroundColor =
+    rgbBlock.style.backgroundColor =
     'rgb('+red + ',' + green + ',' + blue +')';
 
     rgbNumbers.innerHTML =
     red + ', ' + green + ', ' + blue;
+}
+
+// HEX block work
+
+const hexBlock = document.querySelector('.hex-block')
+const hexButton = document.querySelector('.hex-button');
+const hexNumbers = document.querySelector('.hex-numbers');
+
+const hexColors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
+hexButton.addEventListener('click', hexColorChange);
+
+function hexColorChange(){
+    let hex = '#';
+
+    for (let i =0; i<6; i++){
+        let random = Math.floor(Math.random()*hexColors.length);
+        hex += hexColors[random];
+}
+hexNumbers.innerHTML = hex;
+hexBlock.style.backgroundColor = hex;
 }
